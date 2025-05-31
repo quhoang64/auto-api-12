16df4e6c5d89745e618a85017759d09b91684446
Quang Hoang <quhoang01@gmail.com>
2025-05-31 22:07:13 +0700

    day 3 init auto api

---
+++ src/test/java/country/CountriesData.java
@@ -0,0 +1,44 @@
+package country;
+
+public class CountriesData {
+    public static final String ALL_COUNTRIES_DATA = """
+            [
+                {
+                    "name": "Viet Nam",
+                    "code": "VN"
+                },
+                {
+                    "name": "USA",
+                    "code": "US"
+                },
+                {
+                    "name": "Canada",
+                    "code": "CA"
+                },
+                {
+                    "name": "UK",
+                    "code": "GB"
+                },
+                {
+                    "name": "France",
+                    "code": "FR"
+                },
+                {
+                    "name": "Japan",
+                    "code": "JP"
+                },
+                {
+                    "name": "India",
+                    "code": "IN"
+                },
+                {
+                    "name": "China",
+                    "code": "CN"
+                },
+                {
+                    "name": "Brazil",
+                    "code": "BR"
+                }
+            ]
+            """;
+}
---
+++ src/test/java/country/CountryTests.java
@@ -0,0 +1,62 @@
+package country;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class CountryTests {
+
+    @BeforeAll
+    static void setUp(){
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 3000;
+    }
+
+    @Test
+    void verifySchemaGetCountries(){
+        RestAssured.given().log().all()
+                .get("/api/v1/countries")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .assertThat().body(matchesJsonSchemaInClasspath("json-schema/countries-schema.json"));
+    }
+
+    @Test
+    void verifyGetCountriesData() throws JsonProcessingException {
+        Response response = RestAssured.given().log().all()
+                .get("/api/v1/countries");
+
+        // 1. Verify status code
+        response.then().log().all().statusCode(200);
+
+        // 2. Verify header
+        response.then().header("X-Powered-By", equalTo("Express"))
+                .header("Content-Type", equalTo("application/json; charset=utf-8"));
+
+        // 3. Verify Body
+        ObjectMapper mapper = new ObjectMapper();
+        List<Map<String, String>> expected = mapper.readValue(CountriesData.ALL_COUNTRIES_DATA,new TypeReference<List<Map<String, String>>>(){
+        });
+
+        List<Map<String, String>> actual = response.body().as(new TypeRef<List<Map<String, String>>>() {
+        });
+        assertThat(actual.size(), equalTo(expected.size()));
+        assertThat(actual.containsAll(expected), equalTo(true) );
+        assertThat(expected.containsAll(actual), equalTo(true) );
+
+    }
+
+}
