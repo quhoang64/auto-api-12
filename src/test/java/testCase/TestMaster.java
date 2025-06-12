8f050d90bf4f09f3f654796acb8d44837841bf27
Quang Hoang <quhoang01@gmail.com>
2025-06-12 21:43:21 +0700

    refector refresh token then handle timeout

--- src/test/java/testCase/TestMaster.java
+++ src/test/java/testCase/TestMaster.java
@@ -1,15 +1,42 @@
 package testCase;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import model.login.LoginRequest;
+import model.login.LoginResponse;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
-import static utils.ConstantUtils.HOST;
-import static utils.ConstantUtils.PORT;
+import java.time.LocalDateTime;
+
+import static utils.ConstantUtils.*;
 
 public class TestMaster {
+    public static String token;
+    private static int tokenTimeout;
+    private static LocalDateTime retrieveTokenMoment;
+
     @BeforeAll
-    static void globalSetUp(){
+    static void globalSetUp() {
         RestAssured.baseURI = HOST;
         RestAssured.port = PORT;
     }
+
+    @BeforeEach
+    public void globalForEach() {
+        if (StringUtils.isAllBlank(token) || retrieveTokenMoment.plusNanos(tokenTimeout).isAfter(LocalDateTime.now())) {
+            LoginRequest loginRequest = new LoginRequest("staff", "1234567890");
+            Response response = RestAssured.given().log().all()
+                    .header(CONTENT_TYPE_HEADER, REQUEST_CONTENT_TYPE_HEADER_VALUE)
+                    .body(loginRequest)
+                    .post(LOGIN_API);
+            response.then().log().all().statusCode(200);
+            LoginResponse loginResponse = response.body().as(LoginResponse.class);
+            token = loginResponse.getToken();
+            tokenTimeout = loginResponse.getTimeout() * 80 / 100;
+            retrieveTokenMoment = LocalDateTime.now();
+        }
+    }
+
 }
--- src/test/java/testCase/user/CreateUserTest.java
+++ src/test/java/testCase/user/CreateUserTest.java
@@ -24,15 +24,7 @@ import static utils.DateTimeUtils.parseTimeToCurrentTimeZone;
 public class CreateUserTest extends TestMaster {
     @Test
     void verifyCreateUserSuccessful() {
-        LoginRequest loginRequest = new LoginRequest("staff", "1234567890");
-        Response response = RestAssured.given().log().all()
-                .header(CONTENT_TYPE_HEADER, REQUEST_CONTENT_TYPE_HEADER_VALUE)
-                .body(loginRequest)
-                .post(LOGIN_API);
-        // 1. Verify status
-        response.then().log().all().statusCode(200);
 
-        LoginResponse loginResponse = response.body().as(LoginResponse.class);
         // Create UserAddress
         UserAddressRequest userAddressRequest = new UserAddressRequest();
         userAddressRequest.setStreetNumber("123");
@@ -57,7 +49,7 @@ public class CreateUserTest extends TestMaster {
 
         Response createUserResponse = RestAssured.given().log().all()
                 .header(CONTENT_TYPE_HEADER, REQUEST_CONTENT_TYPE_HEADER_VALUE)
-                .header(AUTHORIZATION_HEADER, String.format("Bearer %s", loginResponse.getToken()))
+                .header(AUTHORIZATION_HEADER, String.format("Bearer %s", token))
                 .body(userRequest)
                 .post(CREATE_USER_API);
 
@@ -76,7 +68,7 @@ public class CreateUserTest extends TestMaster {
         // 5. double check that user existing in system or not by getUserAPI
         Response getUserResponse = RestAssured.given().log().all()
                 .header(CONTENT_TYPE_HEADER, REQUEST_CONTENT_TYPE_HEADER_VALUE)
-                .header(AUTHORIZATION_HEADER, String.format("Bearer %s", loginResponse.getToken()))
+                .header(AUTHORIZATION_HEADER, String.format("Bearer %s", token))
                 .get(GET_USER_API, userResponse.getId());
 
         // verify status
@@ -92,21 +84,23 @@ public class CreateUserTest extends TestMaster {
 
         LocalDateTime userCreateAtDate = parseTimeToCurrentTimeZone(actualGetUserResponse.getCreatedAt());
         LocalDateTime userUpdateAtDate = parseTimeToCurrentTimeZone(actualGetUserResponse.getUpdatedAt());
-        assertThat(userCreateAtDate.isAfter(timeBeforeCreate),equalTo(true));
-        assertThat(userCreateAtDate.isBefore(timeAfterCreate),equalTo(true));
-        assertThat(userUpdateAtDate.isAfter(timeBeforeCreate),equalTo(true));
-        assertThat(userUpdateAtDate.isBefore(timeAfterCreate),equalTo(true));
+        assertThat(userCreateAtDate.isAfter(timeBeforeCreate), equalTo(true));
+        assertThat(userCreateAtDate.isBefore(timeAfterCreate), equalTo(true));
+        assertThat(userUpdateAtDate.isAfter(timeBeforeCreate), equalTo(true));
+        assertThat(userUpdateAtDate.isBefore(timeAfterCreate), equalTo(true));
 
-        for(GetUserAddressResponse addressResponse : actualGetUserResponse.getAddresses()){
+        for (GetUserAddressResponse addressResponse : actualGetUserResponse.getAddresses()) {
             assertThat(addressResponse.getCustomerId(), equalTo(userResponse.getId()));
             assertThat(addressResponse.getId(), not(emptyOrNullString()));
 
-            LocalDateTime userAddressCreateAtDate = parseTimeToCurrentTimeZone(addressResponse.getCreatedAt());;
-            LocalDateTime userAddressUpdateAtDate = parseTimeToCurrentTimeZone(addressResponse.getUpdatedAt());;
-            assertThat(userAddressCreateAtDate.isAfter(timeBeforeCreate),equalTo(true));
-            assertThat(userAddressCreateAtDate.isBefore(timeAfterCreate),equalTo(true));
-            assertThat(userAddressUpdateAtDate.isAfter(timeBeforeCreate),equalTo(true));
-            assertThat(userAddressUpdateAtDate.isBefore(timeAfterCreate),equalTo(true));
+            LocalDateTime userAddressCreateAtDate = parseTimeToCurrentTimeZone(addressResponse.getCreatedAt());
+            ;
+            LocalDateTime userAddressUpdateAtDate = parseTimeToCurrentTimeZone(addressResponse.getUpdatedAt());
+            ;
+            assertThat(userAddressCreateAtDate.isAfter(timeBeforeCreate), equalTo(true));
+            assertThat(userAddressCreateAtDate.isBefore(timeAfterCreate), equalTo(true));
+            assertThat(userAddressUpdateAtDate.isAfter(timeBeforeCreate), equalTo(true));
+            assertThat(userAddressUpdateAtDate.isBefore(timeAfterCreate), equalTo(true));
 
 
         }
